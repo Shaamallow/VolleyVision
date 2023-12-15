238332c0ba0267ab4e0cc48bfcd4fc33149d25d2
Shaamallow <eyal.benaroche@gmail.com>
2023-12-15 14:47:21 +0100

    new data -> new_data
    change folder name

--- src/Court_Detection/court_detect.py
+++ src/Court_Detection/court_detect.py
@@ -1,11 +1,8 @@
 import os
-import io
 import cv2
-import base64
 import argparse
 import numpy as np
 from tqdm import tqdm
-import ultralytics
 from ultralytics import YOLO
 import torch
 
@@ -49,43 +46,65 @@ def process_image(input_path, output_path, model):
 
 def process_video(input_path, output_path, model):
     # Load the video file
-    video_capture = cv2.VideoCapture(input_path)
-
-    # Get the video properties
-    frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = video_capture.get(cv2.CAP_PROP_FPS)
-    total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap = cv2.VideoCapture(input_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_size = (
+        int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+    )
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # Define the codec for the output video
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # pyright: ignore
-    output_video = cv2.VideoWriter(
-        output_path, fourcc, fps, (frame_width, frame_height)
-    )
+    out = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
 
     # Loop through each frame of the video
     for _ in tqdm(range(total_frames), desc="Processing video"):
         # Read a frame from the video
-        ret, frame = video_capture.read()
+        ret, frame = cap.read()
         if not ret:
             break
 
         # Process
-        cv2.imwrite("temp.jpg", frame)
-        process_image("temp.jpg", "temp_processed.jpg", model)
-        frame = cv2.imread("temp_processed.jpg")
+        image = np.array(frame)
+        image_copy = image.copy()
+        image_copy = cv2.resize(image_copy, (640, 640))
+        results = model(image_copy)
 
-        raise Exception("test")
+        result = results[0]
+        # get array results
+        masks = result.masks.data
+        boxes = result.boxes.data
+        # extract classes
+        clss = boxes[:, 5]
+        # get indices of results where class is 0
+        court_indices = torch.where(clss == 0)
+        # use these indices to extract the relevant masks
+        court_masks = masks[court_indices[0]]
 
-        # Write the modified frame to the output video
-        output_video.write(frame)
+        # scale for visualizing results (optional)
+        court_mask = torch.any(court_masks, dim=0).int() * 255
+        mask_image = court_mask.cpu().numpy()
+        # convert to CV_8UC1 image
+        mask_image = mask_image.astype(np.uint8)
+        # Resize the mask image to the original image size
+        mask_image = cv2.resize(mask_image, (frame_size[0], frame_size[1]))
+
+        # Find and Draw Contours
+        contours, _ = cv2.findContours(
+            mask_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        largest_contour = max(contours, key=cv2.contourArea)
+        epsilon = 0.01 * cv2.arcLength(largest_contour, True)
+        trapezoid = cv2.approxPolyDP(largest_contour, epsilon, True)
+
+        cv2.drawContours(image, [trapezoid], 0, (0, 0, 0), 5)
+        out.write(image)
 
     # Release the video capture and output video
-    video_capture.release()
-    output_video.release()
-
-    # Destroy any remaining windows
+    cap.release()
     cv2.destroyAllWindows()
+    out.release()
 
     # Delete temporary files
     if os.path.exists("temp.jpg"):
@@ -98,7 +117,10 @@ if __name__ == "__main__":
     # Define and parse command-line arguments
     parser = argparse.ArgumentParser(description="Process an image or a video.")
     parser.add_argument(
-        "input_path", type=str, help="Path to the input video or image."
+        "--input_path",
+        type=str,
+        default="data/Court_Detection/modena_court.jpg",
+        help="Path to the input video or image.",
     )
     parser.add_argument(
         "--output_path",
