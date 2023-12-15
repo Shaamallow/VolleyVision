61ad7e0dacd933b7e3775fd47765a0565b002851
Shaamallow <eyal.benaroche@gmail.com>
2023-12-15 17:19:32 +0100

    Clean Workspace
    remove useless files
    - former doc
    - former models
    - simplify folder structure

--- src/Tracking/train.py
+++ src/Tracking/train.py
@@ -3,5 +3,5 @@ import os
 
 cwd = os.getcwd()
 
-model = YOLO(cwd + "/models/yolov8/yolov8n.pt")
-results = model.train(data=cwd + "/data/Tracking/dataset/data.yaml", epochs=100)
+model = YOLO("models/yolov8/yolov8n.pt")
+results = model.train("data/Tracking/dataset/data.yaml", epochs=100)
